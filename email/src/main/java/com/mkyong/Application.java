5d5447d16fa5e34eeac20821dfa951f0b8503f6d
dcremona <davide.cremona@gmail.com>
2022-04-15 14:15:14 +0200

    fix

--- email/src/main/java/com/mkyong/Application.java
+++ email/src/main/java/com/mkyong/Application.java
@@ -1,6 +1,8 @@
 package com.mkyong;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -11,15 +13,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
+
 
 @SpringBootApplication
 public class Application implements CommandLineRunner{
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+	@Autowired
+	private Environment env;
 
 	// https://docs.spring.io/spring/docs/5.1.6.RELEASE/spring-framework-reference/integration.html#mail
 	@Autowired
@@ -27,21 +36,17 @@ public class Application implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
 
-		// SpringApplicationBuilder builder = new
-		// SpringApplicationBuilder(Application.class);
-		// builder.headless(false);
-		// ConfigurableApplicationContext context = builder.run(args);
-		// LOGGER.info(context.getDisplayName());
-
-		// LOGGER.info("Simple log statement with inputs {}, {} and {}", 1, 2,
-		// 3);
-		// String fonts[] =
-		// GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-		// LOGGER.info("fonts " + fonts.length);
-		// for (String s : fonts) {
-		// LOGGER.info(s);
-		// }
+	public static String[] tornaArrayString(String sArray, String div) {
+		StringTokenizer st = new StringTokenizer(sArray,div);
+		String[] vet = new String[st.countTokens()];
+		int conta = 0;
+		while (st.hasMoreTokens()) {
+			vet[conta] = st.nextToken().toString();
+			conta++;
+		}
+		return vet;
 	}
 
 	@Override
@@ -49,15 +54,72 @@ public class Application implements CommandLineRunner{
 
 		System.out.println("Sending Email..");
 
+		String springMailPassword = (String) env.getProperty("spring.mail.password");
+		System.out.println("springMailPassword " + springMailPassword);
+
 		try {
-			sendEmail();
-			sendEmailWithAttachment();
+			
+			MimeMessage msg = javaMailSender.createMimeMessage();
+			msg.setFrom("notifiche-fclt@hostingtt.it");
+			
+			// use the true flag to indicate you need a multipart message
+			MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+			helper.setTo("davcic@libero.it");
+			helper.setCc("davide.cremona@gmail.com");
+
+			// use the true flag to indicate the text included is HTML
+			helper.setText("<html><body><img src='cid:identifier1234'></body></html>", true);
+
+			// let's include the infamous windows Sample file (this time copied to c:/)
+			FileSystemResource res = new FileSystemResource(new File("/app/android.png"));
+			helper.addInline("identifier1234", res);
+
+			javaMailSender.send(msg);
+			
+			// sendEmail();
+			// sendEmailWithAttachment();
+
+//			Properties p = new Properties();
+//			p.setProperty("from", "notifiche-fclt@hostingtt.it");
+//			p.setProperty("to", "davcic@libero.it");
+//
+//			MailClient client = new MailClient(javaMailSender);
+//
+//			String fromS = (String) p.getProperty("from");
+//			String toS = (String) p.getProperty("to");
+//
+//			String[] to = null;
+//			if (toS != null && !toS.equals("")) {
+//				to = tornaArrayString(toS, ";");
+//			}
+//
+//			String subject = "Test Ogg";
+//
+//			String cid = ContentIdGenerator.getContentId();
+//
+//			String message = "<html><head>" + "<title>This is not usually" + "displayed</title>" + "</head>\n" + "<body><div><b>Hi there!</b></div>" 
+//			+ "<div>Sending HTML in email is so <i>cool!</i> </div>\n" + "<div>And" + "here's an image: <img src=\"cid:" + cid + "\" /></div>\n" + "<div>I hope" + "you like it!</div></body></html>";
+//			
+//			String[] cc = null;
+//			String[] bcc = null;
+//			String[] att = new String[] { "/app/android.png" };
+//
+//			// client.sendMail(fromS, to, cc, bcc, subject, message,
+//			// "text/html", "1", att);
+//
+//			File file = new File("/app/android.png");
+//			InputStream targetStream = new FileInputStream(file);
+//
+//			Map<String, InputStream> images = new HashMap<String, InputStream>();
+//			images.put(cid, targetStream);
+//			client.sendMail2(fromS, to, cc, bcc, subject, message, "text/html", "1", images);
+
 		} catch (MessagingException e) {
 			e.printStackTrace();
 			LOGGER.error(e.getMessage());
-		} catch (IOException e) {
-			e.printStackTrace();
-			LOGGER.error(e.getMessage());
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			LOGGER.error(e.getMessage());
 		}
 
 		System.out.println("Done");
---
+++ email/src/main/java/com/mkyong/MailClient.java
@@ -0,0 +1,209 @@
+package com.mkyong;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+public class MailClient{
+
+	private static final Logger log = LoggerFactory.getLogger(MailClient.class);
+
+	private JavaMailSender javaMailSender;
+
+	public MailClient(JavaMailSender jMS) {
+		javaMailSender = jMS;
+	}
+
+	public void sendMail(String from, String[] to, String[] cc, String[] bcc,
+			String subject, String messageBody, String typeMessage,
+			String priority, String[] attachments)
+			throws IOException, MessagingException, AddressException {
+
+		log.info("****************************************");
+		log.info("INIZIO ESECUZIONE sendMail              ");
+		log.info("****************************************");
+
+		log.info(" from: " + from);
+		log.info(" subject: " + subject);
+		log.info(" message: " + messageBody);
+
+		if (attachments != null) {
+
+			MimeMessage msg = javaMailSender.createMimeMessage();
+			msg.setFrom(from);
+
+			MimeMessageHelper helper = new MimeMessageHelper(msg,true);
+			helper.setTo(to);
+			if (cc != null) {
+				helper.setCc(cc);
+			}
+			if (bcc != null) {
+				helper.setBcc(bcc);
+			}
+
+			helper.setSubject(subject);
+			if (typeMessage.equals("text/html")) {
+				helper.setText(messageBody, true);
+			} else {
+				msg.setText(messageBody);
+			}
+
+			for (int i = 0; i <= attachments.length - 1; i++) {
+				File file = new File(attachments[i]);
+				helper.addAttachment(file.getName(), file);
+			}
+
+			javaMailSender.send(msg);
+
+		} else {
+
+			if (typeMessage.equals("text/html")) {
+
+				MimeMessage msg = javaMailSender.createMimeMessage();
+				msg.setFrom(from);
+				MimeMessageHelper helper = new MimeMessageHelper(msg,true);
+				helper.setTo(to);
+				if (cc != null) {
+					helper.setCc(cc);
+				}
+				if (bcc != null) {
+					helper.setBcc(bcc);
+				}
+				helper.setSubject(subject);
+				helper.setText(messageBody, true);
+				javaMailSender.send(msg);
+
+			} else {
+
+				SimpleMailMessage msg = new SimpleMailMessage();
+				msg.setFrom(from);
+				msg.setTo(to);
+				if (cc != null) {
+					msg.setCc(cc);
+				}
+				if (bcc != null) {
+					msg.setBcc(bcc);
+				}
+				msg.setSubject(subject);
+				msg.setText(messageBody);
+				javaMailSender.send(msg);
+			}
+		}
+
+		log.info("****************************************");
+		log.info("FINE ESECUZIONE sendMail              ");
+		log.info("****************************************");
+	}
+
+	public void sendMail2(String from, String[] to, String[] cc, String[] bcc,
+			String subject, String messageBody, String typeMessage,
+			String priority, Map<String, InputStream> images)
+			throws IOException, MessagingException, AddressException {
+
+		log.info("****************************************");
+		log.info("INIZIO ESECUZIONE sendMail              ");
+		log.info("****************************************");
+
+		log.info(" from: " + from);
+		log.info(" subject: " + subject);
+		log.info(" message: " + messageBody);
+
+		if (images != null) {
+
+			MimeMessage msg = javaMailSender.createMimeMessage();
+			msg.setFrom(from);
+
+			MimeMessageHelper helper = new MimeMessageHelper(msg,true);
+			helper.setTo(to);
+			if (cc != null) {
+				helper.setCc(cc);
+			}
+			if (bcc != null) {
+				helper.setBcc(bcc);
+			}
+
+			helper.setSubject(subject);
+			if (typeMessage.equals("text/html")) {
+				helper.setText(messageBody, true);
+			} else {
+				msg.setText(messageBody);
+			}
+
+			Iterator<?> it = images.entrySet().iterator();
+			while (it.hasNext()) {
+				@SuppressWarnings("rawtypes")
+				Map.Entry pairs = (Map.Entry) it.next();
+
+				InputStream inputStream = (InputStream) pairs.getValue();
+				File somethingFile = File.createTempFile("test", ".png");
+				try {
+					FileUtils.copyInputStreamToFile(inputStream, somethingFile);
+				} catch (Exception e) {
+					e.printStackTrace();
+					log.error(e.getMessage());
+				} finally {
+					IOUtils.closeQuietly(inputStream);
+				}
+				helper.addInline((String) pairs.getKey(), somethingFile);
+
+			}
+
+			javaMailSender.send(msg);
+
+		} else {
+
+			if (typeMessage.equals("text/html")) {
+
+				MimeMessage msg = javaMailSender.createMimeMessage();
+				msg.setFrom(from);
+				MimeMessageHelper helper = new MimeMessageHelper(msg,true);
+				helper.setTo(to);
+				if (cc != null) {
+					helper.setCc(cc);
+				}
+				if (bcc != null) {
+					helper.setBcc(bcc);
+				}
+				helper.setSubject(subject);
+				helper.setText(messageBody, true);
+				javaMailSender.send(msg);
+
+			} else {
+
+				SimpleMailMessage msg = new SimpleMailMessage();
+				msg.setFrom(from);
+				msg.setTo(to);
+				if (cc != null) {
+					msg.setCc(cc);
+				}
+				if (bcc != null) {
+					msg.setBcc(bcc);
+				}
+				msg.setSubject(subject);
+				msg.setText(messageBody);
+				javaMailSender.send(msg);
+
+			}
+		}
+
+		log.info("****************************************");
+		log.info("FINE ESECUZIONE sendMail              ");
+		log.info("****************************************");
+
+	}
+
+}
